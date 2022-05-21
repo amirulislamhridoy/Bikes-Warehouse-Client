d9be37e083afc7b5d2910894f6e013ef5b5b7258
Your Name <hossainhridoy@30687@gmail.com>
2022-05-22 00:08:43 +0600

    update button compleate

--- src/pages/Update/Update.js
+++ src/pages/Update/Update.js
@@ -3,19 +3,33 @@ import { Link, useParams } from "react-router-dom";
 
 const Update = () => {
   const { id } = useParams();
-  const [bike, setBikes] = useState({});
+  const [bike, setBike] = useState({});
 
   useEffect(() => {
     fetch(`http://localhost:5000/bike/${id}`)
       .then((res) => res.json())
-      .then((data) => setBikes(data));
-  }, []);
-  
-  const handleDelivery = (bike) => {
-    const quantity = parseInt(bike.quantity) + 1
+      .then((data) => setBike(data));
+  }, [bike]);
 
-    
-  }
+  const handleDelivery = (bike) => {
+    const quantity = parseInt(bike.quantity) - 1;
+    const id = bike._id
+    const data = {quantity, id}
+
+    fetch("http://localhost:5000/bike", {
+      method: "PUT",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => { 
+        setBike(bike.quantity = quantity)
+        console.log(json)
+        console.log(bike)
+      });
+  };
   return (
     <div className="container">
       <div className="card border-0" key={bike._id}>
@@ -26,7 +40,12 @@ const Update = () => {
           <h5>Quantity: {bike.quantity}</h5>
           <h5>Taka: {bike.price}</h5>
           <h5>Supplier: {bike.supplierName}</h5>
-          <button onClick={() => handleDelivery(bike)} className="btn btn-primary">Delivery</button>
+          <button
+            onClick={() => handleDelivery(bike)}
+            className="btn btn-primary"
+          >
+            Delivery
+          </button>
           {/* <Link to={`/update/${bike._id}`} className="btn btn-primary">
             Stock Update
           </Link> */}
