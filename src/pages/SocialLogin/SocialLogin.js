7e4ab0028357de2a8727453e7d6f745f44020618
Your Name <hossainhridoy@30687@gmail.com>
2022-05-22 10:57:30 +0600

    social login

--- src/pages/SocialLogin/SocialLogin.js
+++ src/pages/SocialLogin/SocialLogin.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import google from "../../photos/socialLogin/google.png";
+import facebook from "../../photos/socialLogin/facebook.png";
+import github from "../../photos/socialLogin/github.png";
+import "./SocialLogin.css";
+import auth from "../../firebase_init";
+import { useSignInWithFacebook, useSignInWithGithub, useSignInWithGoogle } from "react-firebase-hooks/auth";
 
 const SocialLogin = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [signInWithGithub, githubUser, githubLoading, githubError] = useSignInWithGithub(auth);
+    const [signInWithFacebook, facebookUser, facebookLoading, facebookError] = useSignInWithFacebook(auth);
+  return (
+    <div className="mt-3">
+      <button onClick={() => signInWithGoogle()} className="btn google-btn me-5">
+        <img width="30px" className="" src={google} alt="" />
+        <span className="mx-2">Google with Login</span>
+      </button>
+      <button onClick={() => signInWithFacebook()} className=" btn facebook-btn me-5">
+        <img width="30px" className="" src={facebook} alt="" />
+        <span className="mx-2">Facebook with Login</span>
+      </button>
+      <button onClick={() => signInWithGithub()} className=" btn github-btn">
+        <img width="30px" className="" src={github} alt="" />
+        <span className="mx-2">Github with Login</span>
+      </button>
+    </div>
+  );
 };
 
 export default SocialLogin;
